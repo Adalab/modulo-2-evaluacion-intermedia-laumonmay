b0c98cdfcebe6c8a4bcf184af954a6edcc173d3c
laumonmay <laumonmay@hotmail.com>
2024-01-05 11:35:47 +0100

    actualizacion

--- js/main.js
+++ js/main.js
@@ -1,59 +1,79 @@
-'use strict'
+"use strict";
 
-// QUERY-SELECTOR
+// 1. QUERY-SELECTOR
 
-const numberInput = document.querySelector('.js_numberInput');  //input
-const button = document.querySelector('.js_button');   //boton prueba
+const numberInput = document.querySelector(".js_numberInput"); //input
+const button = document.querySelector(".js_button"); //boton
+const clue = document.querySelector(".js_clue"); // pistA
+const opportunitiesBox = document.querySelector(".js_opportunitiesBox");
 
+// 2. VARIABLES GLOBALES:
 
+const randomNumber = getRandomNumber(100); // variable para guardar el nº aleatorio al llamar a la funcion.
 
-// CUANDO CARGA LA PÁGINA 
+let opportunityCounter = 0; // creo variable para guardar el intento origen 0
 
-//nº aleatorio
+// 3. FUNCIONES
 
-const numeroAleatorio = getRandomNumber (100);
-
-
-// FUNCIONES
-
+// Punto 2. Generar un número aleatorio con la ayuda de Math.random y Math.ceil. Función para crear numero aleatorio:
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-    
+  return Math.ceil(Math.random() * max);
 }
-console.log(numeroAleatorio);
-const cambiarPista = () => {
 
-};
-const cambiarIntentos = () => {
+// funcion para recoger el valor del input:
+function getNumberInput() {
+  return parseInt(numberInput.value); // convertimos el string en nº
+}
 
-};
-
-// EVENTOS
-
-
-
-//funcion para modificar las pistas
-
-const pista = (event) => {
-    event.preventDefault();
-
-    recogerValue();
+// funcion para Comparar el nº de la usuaria con el nº aleatorio:
+function checkNumbers(userNumber, randomNumber) {
  
-    cambiarPista();
+   // Punto 5. Comparar si el numeroUsuaria con el numeroAleatorio, y pintar las pistas correspondientes en la pantalla.
 
-    cambiarIntentos();
+   if (userNumber < 1 || userNumber > 100) {
+    giveClue(`El número debe estar entre 1 y 100.`); // llamo a la funcion para introducir este argumento
+  } else if (userNumber < randomNumber) {
+    giveClue(`Pista: Demasiado bajo.`);
+  } else if (userNumber > randomNumber) {
+    giveClue(`Pista: Demasiado alto.`);
+  } else {
+    giveClue(`Has ganado campeona!!!`);
+  }
 };
 
-button.addEventListener('click', (event) => {
+// función para cambiar el contenido de clue por message:
 
-    const numeroUsuaria = numberInput.value;
+function giveClue (message) {
+    clue.innerHTML = message;
+}
 
-    if (numeroUsuaria < numeroAleatorio)
-    
-});    /// al hacer click
+// función contador:
+
+function opportunities(){
+    opportunityCounter++;  // le sumo 1
+    opportunitiesBox.innerHTML = `Número de intentos: ${opportunityCounter}`; // cambio el innerHTML para actualizarlo
+}
+
+ 
+// 4. EVENTOS
+
+//Punto 4. Al pulsar el botón de Prueba, acceder al contenido del input y mostrarlo en la consola.
+
+//creo una función handleClick (manejadora)
+function handleClick(event) {
+  const userNumber = getNumberInput(); // guardar en variable el valor de la función
+  checkNumbers(userNumber, randomNumber); // llamo a la funcion para comparar los números
+  opportunities(); // llamo a la funcion para contar los intentos
+}
+
+
+button.addEventListener("click", handleClick);
 
 
 
+// 5. CÓDIGO QUE SE EJECUTA CUANDO CARGA LA PÁGINA
 
+// Punto 3. Mostrar en consola el número aleatorio generado para poder saber qué número se ha generado y poder hacer pruebas.
 
+console.log(randomNumber);
